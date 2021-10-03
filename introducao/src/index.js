7d7c0468c64b7cab43b1ddbf1ddcca0c3e6f217a
Igor Santana <igorsantana@alu.ufc.br>
2021-10-03 15:35:36 -0300

    Primeiro Commit

---
+++ introducao/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom'
+import OlaMundo from './components/OlaMundo'
+import IgorSantana from './components/IgorSantana'
+
+
+const root = document.getElementById('root')
+ReactDOM.render(<IgorSantana/>, root)
